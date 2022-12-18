03944d83a1f468fc97e238e0d59682630818a047
UMUT <yildizumutcan21@gmail.com>
2022-12-19 01:20:04 +0300

    silder yapildi

--- home/views.py
+++ home/views.py
@@ -1,13 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from Work.models import work
 from home.models import Setting
 
 
 # Create your views here.
 def index(request):
     setting = Setting.objects.get(pk = 1)
-    context = {'setting': setting, 'page': 'home'}
+    sliderdata= work.objects.all()[:4]
+    context = {'setting': setting, 'page': 'home','sliderdata': sliderdata}
     return render(request, 'index.html', context)
 
 def hakkimizda(request):
