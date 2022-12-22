446b3c87a6e76947add04e27b64b4a37a39055b3
UMUT <yildizumutcan21@gmail.com>
2022-12-22 23:30:40 +0300

    kullanıcı profil sayfasi yapildi

---
+++ user/views.py
@@ -0,0 +1,23 @@
+from Work.models import Category, work, Images
+
+from home.models import Setting, UserProfile
+
+from django.shortcuts import render
+
+
+def index(request):
+    if request.user.id is not None:
+        category = Category.objects.all()
+        user = UserProfile.objects.get(user_id=request.user.id)
+        settings = Setting.objects.filter(pk=0)
+        categorydata = Category.objects.all ()
+        setting = Setting.objects.get (pk=1)
+        category = Category.objects.all ()
+
+        context = {'category': category,
+                   'user': user,
+                   'settings': settings,
+                   'setting': setting,
+                   'categorydata': categorydata
+                   }
+        return render(request, 'user_profile.html', context)
